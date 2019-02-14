729276c41a62435a0b21b89e905b5680fc635fb3
Valtteri Kodisto <42912038+valtterikodisto@users.noreply.github.com>
2019-02-14 16:01:28 +0200

    Added exercise 4.4 and 4.5

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,28 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  const reducer = (sum, blog) => {
+    return sum + blog.likes
+  }
+
+  return blogs.reduce(reducer, 0)
+}
+
+const favoriteBlog = (blogs) => {
+  const reducer = (prev, current) => {
+    return prev.likes > current.likes
+      ? prev
+      : current
+  }
+
+  const blog = blogs.reduce(reducer, {likes: -1})
+
+  return blog.likes !== -1 ? blog : undefined
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes,
+  favoriteBlog
 }
