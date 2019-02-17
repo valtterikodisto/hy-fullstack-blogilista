11be02b986938031a0d349dc073b4447e35b6751
Valtteri Kodisto <42912038+valtterikodisto@users.noreply.github.com>
2019-02-17 17:14:09 +0200

    Added exercises 4.13 and 4.14

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -30,4 +30,32 @@ blogsRouter.post('/', (request, response, next) => {
     .catch(error => next(error))
 })
 
+blogsRouter.delete('/:id', async (request, response, next) => {
+  try {
+    await Blog.findByIdAndDelete(request.params.id)
+    response.status(204).end()
+  } catch(error) {
+    next(error)
+  }
+})
+
+blogsRouter.put('/:id', async (request, response, next) => {
+  const body = request.body
+
+  const blog = {
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes
+  }
+
+  try {
+    const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+    response.json(updatedBlog.toJSON())
+
+  } catch(error) {
+    next(error)
+  }
+})
+
 module.exports = blogsRouter
